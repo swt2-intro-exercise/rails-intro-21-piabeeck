f1a34fa47869d414787135941215dce34e9dfcf3
piabeeck <hellopia@gmx.de>
2021-11-19 14:30:51 +0100

    fixes #20

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,8 @@ class Author < ApplicationRecord
     validates :last_name, presence: true
     validates :homepage, presence: true #03: validation declares that a first_name value must be present
 
+    has_and_belongs_to_many :paper
+
     def name # 03
         full_name = self.first_name + " " + self.last_name
         return full_name
